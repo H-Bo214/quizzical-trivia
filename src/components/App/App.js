47f0f9a3629cdceda44a27111b41c207492d5926
Horacio Borrego <hborrego365@gmail.com>
2022-03-02 13:53:25 -0800

    Install react router, import favicon, create components folder, create HomePage folder, create Questions folder, create Homepage general component, create Questions general component, Add BrowerRouter to index.js, Add routes to HomePage, Questions and error path, remove unecessary create React App files

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../HomePage/HomePage'
+import Questions from '../Questions/Questions'
+
+function App() {
+  return (
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='questions' element={<Questions />} />
+          <Route path='*' element={<div>An error occurred.</div>} />
+        </Routes>
+
+  );
+}
+
+export default App;
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+  return (
+    <main>
+      <h1>I am on the HomePage</h1>
+    </main>
+  )
+}
+
+export default HomePage
