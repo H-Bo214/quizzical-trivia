a2cef1e36626e8a7993c9d180061329194d3eb9a
Horacio Borrego <hborrego365@gmail.com>
2022-03-03 12:01:16 -0800

    Move state from HomePage component to Questions component, fetch questions from API, Add function to sanitize data received from API, add shuffleArray function to shuffle question answers, create state in Questions component for questions, create error state in Questions componet

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,24 +1,7 @@
-
-// pass a function to call the API for the questions to the onClick property of the button.
-// Once we receive the response from the API, store these values in state by using useState()
 import { Link } from 'react-router-dom'
 import Button from '../Button/Button'
-import { useState, useEffect } from 'react'
 
 const HomePage = () => {
-  const [questions, setQuestions] = useState([])
-  const [error, setError] = useState('')
-  const questionsAPI ='https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple'
-
-  useEffect(()=> {
-    return fetch(questionsAPI)
-    .then(response => response.json())
-    .then(data => setQuestions(data.results))
-    .catch(err => setError(err))
-    
-  }, [])
-  console.log('questions', questions)
-
   return (
     <main>
       <h1>Quizzical trivia</h1>
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from 'react'
+
 const Questions = () => {
+
+  const [questions, setQuestions] = useState([])
+  const [error, setError] = useState('')
+
+  const questionsAPI ='https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple'
+
+  useEffect(()=> {
+    return fetch(questionsAPI)
+    .then(response => response.json())
+    .then(data => setQuestions(cleanData(data.results)))
+    .catch(err => setError(err))
+    
+  }, [])
+
+  const shuffleArray = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+  }
+
+  const cleanData = (data) => {
+    const result = data.map(question => {
+      return {
+        question: question.question,
+        correctAnswer: question.correct_answer,
+        allAnswers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+      }
+    })
+    return result
+  }
+
   return (
     <div>
-      <h2>This is the Questions page</h2>
+      {error && <h1>An error occurred</h1>}
     </div>
   )
 }
