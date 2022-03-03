b450a7ac941d235dc1c2dcf07e64911b1324ffc2
Horacio Borrego <hborrego365@gmail.com>
2022-03-02 17:02:50 -0800

    Add index to HomePage router path, create Button component, render button tag,  add props bckgrdColor, textColor, btnText, btnClassName, add Button PropTypes, import Button component into HomePage component, pass down props to Button component in HomePage, add Link to route user to questions page onClick of the Start quiz button

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,26 @@
-
-
+// render a Button component
+// pass a function to call the API for the questions to the onClick property of the button.
+// Once we receive the response from the API, store these values in state by using useState()
+import Button from '../Button/Button'
+import { Link } from 'react-router-dom'
 const HomePage = () => {
+
+  function handleFetch() {
+    console.log('Button click successful')
+  }
+
   return (
     <main>
-      <h1>I am on the HomePage</h1>
+      <h1>Quizzical trivia</h1>
+      <p>Test your knowledge of video games!</p>
+    <Link to="/questions">
+      <Button 
+        btnText={'Start quiz'}
+        bckgrdColor={'#4D5B9E'}
+        textColor={'#F5F7FB'}
+        onClick={handleFetch}
+      />
+    </Link>
     </main>
   )
 }
