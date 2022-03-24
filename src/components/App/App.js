c62b87e9eb5b72d713813f608fb7da3cb0f5f2a9
Horacio Borrego <hborrego365@gmail.com>
2022-03-24 09:38:28 -0700

    Remove header and logo, Remove unused files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from '../HomePage/HomePage'
 import Questions from '../Questions/Questions'
 import Results from '../Results/Results'
-import Header from '../Header/Header'
 
 function App() {
   return (<>
-        <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='questions' element={<Questions />} />
